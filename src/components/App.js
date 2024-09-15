772f1d31398376c091c4bdd2882cc54ed0d93ce5
manishamali1525 <manishamali1525@users.noreply.github.com>
2024-09-15 19:37:16 +0530

    updated files

--- src/components/App.js
+++ src/components/App.js
@@ -1,50 +1,53 @@
-import React, {useState} from 'react';
-  
-  const App = () =>  {
+import React, { useState } from 'react';
 
-    const [name1,setName1] = useState("");
-    const [name2,setName2] = useState("");
-    const [result,setResult] = useState("");
+const App = () => {
+  const [name1, setName1] = useState("");
+  const [name2, setName2] = useState("");
+  const [result, setResult] = useState("");
 
-    const relationships = ["Siblings", "Friends", "Love", "Affection", "Marriage", "Enemy"];
+  const relationships = ["Siblings", "Friends", "Love", "Affection", "Marriage", "Enemy"];
 
-    const removeCommonLetters = (str1,str2) =>{
-        let arr1 = str1.split("");
-        let arr2 = str2.split("");
+  const removeCommonLetters = (str1, str2) => {
+    let arr1 = str1.split("");
+    let arr2 = str2.split("");
 
-        arr1.forEach(char =>{
-            if(arr2.includes(char)){
-                arr2.splice(arr2.indexOf(char),1);
-            }else{
-                arr1.splice(arr1.indexOf(char),1);
-            }
-        })
-        return arr1.length+arr2.length;
-    }
-    const calculateRelationship = () =>{
-        if(name1==="" || name2===""){
-            setResult("Please Enter valid input");
-            return;
-        }
-        const lengthAfterRemoval = removeCommonLetters(name1,name2);
-        const index = lengthAfterRemoval % 6;
-        setResult(relationships[index]);
-    }
-    const clearForm = () =>{
-        setName1("");
-        setName2("");
-        setResult("");
-    }
-	return (
-	  <div>
-        <input type="text" data-testid="input1" value={name1} onChange={(e)=>setName1(e.target.value)}/>
-        <input type="text" data-testid="input2" value={name2} onChange={(e)=>setName2(e.target.value)} />
-        <button data-testid="calculate_relationship" onClick={calculateRelationship}>Calculate Relationship Future</button>
-        <button data-testid="clear" onClick={clearForm}>Clear</button>
-        <h3 data-testid="answer">{result}</h3>
-	  </div>
-	);
+    arr1.forEach((char, index) => {
+      if (arr2.includes(char)) {
+        arr2.splice(arr2.indexOf(char), 1); // Remove char from arr2
+        arr1[index] = null; // Mark char as removed in arr1
+      }
+    });
+
+    const filteredArr1 = arr1.filter(char => char !== null);
+    return filteredArr1.length + arr2.length;
   }
-  
-export default App;
 
+  const calculateRelationship = () => {
+    if (name1 === "" || name2 === "") {
+      setResult("Please Enter valid input");
+      return;
+    }
+
+    const lengthAfterRemoval = removeCommonLetters(name1, name2);
+    const index = lengthAfterRemoval % 6;
+    setResult(relationships[index]);
+  }
+
+  const clearForm = () => {
+    setName1("");
+    setName2("");
+    setResult("");
+  }
+
+  return (
+    <div>
+      <input type="text" data-testid="input1" value={name1} onChange={(e) => setName1(e.target.value)} />
+      <input type="text" data-testid="input2" value={name2} onChange={(e) => setName2(e.target.value)} />
+      <button data-testid="calculate_relationship" onClick={calculateRelationship}>Calculate Relationship Future</button>
+      <button data-testid="clear" onClick={clearForm}>Clear</button>
+      <h3 data-testid="answer">{result}</h3>
+    </div>
+  );
+}
+
+export default App;
